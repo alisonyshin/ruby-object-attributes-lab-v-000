c233410d38c0fa86cebec9ab1d8b195dea5c088c
Alison Shin <alisonyshin@gmail.com>
2018-07-22 03:58:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,7 +8,7 @@ class Person
     @name
   end
 
-  def name = (new_name)
+  def name= (new_name)
     @name = new_name
   end
   
@@ -16,7 +16,7 @@ class Person
     @job
   end
   
-  def job = (new_job)
+  def job= (new_job)
     @job = new_job
   end
   
